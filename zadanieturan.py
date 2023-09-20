51b320658c96f258da6dcc486c80f1f80bbd6e08
P00reshka <145564046+P00reshka@users.noreply.github.com>
2023-09-20 20:52:24 +0600

    Add files via upload
    Это третья версия программы

--- zadanieturan.py
+++ zadanieturan.py
@@ -9,3 +9,19 @@ print("*. Умножение")
 print(":. Деление")
 
 choice = input("Введите операцию (+/-/*/:): ")
+
+if choice == '+':
+    result = num1 + num2
+    operation = "сложения"
+elif choice == '-':
+    result = num1 - num2
+    operation = "вычитания"
+elif choice == '*':
+    result = num1 * num2
+    operation = "умножения"
+elif choice == ':':
+        result = num1 / num2
+        operation = "деления"
+
+if result is not None:
+    print(f"Результат {operation}: {result}")
