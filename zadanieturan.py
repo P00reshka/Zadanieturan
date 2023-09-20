cd81eed5a1f83aa18e69dc29014100de8b05ca57
P00reshka <145564046+P00reshka@users.noreply.github.com>
2023-09-20 21:10:35 +0600

    Add files via upload
    Четвёртая часть программы

--- zadanieturan.py
+++ zadanieturan.py
@@ -1,27 +1,40 @@
 #Задание 1
-num1 = float(input("Введите первое число: "))
-num2 = float(input("Введите второе число: "))
+while True:
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
 
-print("Выберите операцию:")
-print("+. Сложение")
-print("-. Вычитание")
-print("*. Умножение")
-print(":. Деление")
+    print("Выберите операцию:")
+    print("+. Сложение")
+    print("-. Вычитание")
+    print("*. Умножение")
+    print(":. Деление")
+    print("End. Выйти")
 
-choice = input("Введите операцию (+/-/*/:): ")
+    choice = input("Введите операцию (+/-/*/:/End): ")
 
-if choice == '+':
-    result = num1 + num2
-    operation = "сложения"
-elif choice == '-':
-    result = num1 - num2
-    operation = "вычитания"
-elif choice == '*':
-    result = num1 * num2
-    operation = "умножения"
-elif choice == ':':
-        result = num1 / num2
-        operation = "деления"
 
-if result is not None:
-    print(f"Результат {operation}: {result}")
+    if choice == 'End':
+        print("Выход из программы.")
+        break
+
+    if choice in ('+', '-', '*', '4'):
+        if choice == '+':
+            result = num1 + num2
+            operation = "сложения"
+        elif choice == '-':
+            result = num1 - num2
+            operation = "вычитания"
+        elif choice == '*':
+            result = num1 * num2
+            operation = "умножения"
+        elif choice == ':':
+            if num2 == 0:
+                print("Ошибка: нельзя делить на ноль")
+                continue
+            else:
+                result = num1 / num2
+                operation = "деления"
+
+        print(f"Результат {operation}: {result}")
+    else:
+        print("Ошибка: неверный выбор операции")
